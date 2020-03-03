bb4e2d79ee3b22417ea576c52b2ddc738600460d
PymatFlow <eeiranmatlab@gmail.com>
2020-03-03 11:01:26 +0330

    Add files via upload

---
+++ aparat_code.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 11:15:35 2019
+
+@author: Kz
+"""
+
+import bs4
+import requests
+
+addres = 'https://www.aparat.com/eeiranmatlab/videos'
+page = requests.get(addres)
+
+cc = page.content
+
+cc1 = page.text
+
+soup = bs4.BeautifulSoup(cc1,'html.parser')
+
+tag1 = soup.find_all("div",{"class":"grid-thumbnail"})
+
+tag2 = tag1[0].find_all("div",{"class":"item grid-item"})
+tag3 = tag1[0].find_all("div",{"class":"thumb-view-date"})
+l =[]
+for item2,item3 in zip(tag2,tag3):
+    dic ={}
+    dic["title"] = item2.find("a",{"class":"title"}).find("span").text
+    dic["num_view"] = item3.text.split("\n")[1].replace("  بازدید","")
+    l.append(dic)
+
+import pandas
+df = pandas.DataFrame(l)
+
